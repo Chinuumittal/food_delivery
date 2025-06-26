cfdd17a827af043a5767a54c7799caa3f7227f03
Chinu Mittal <167022648+Chinuumittal@users.noreply.github.com>
2025-06-26 15:16:50 -0300

    Update Storecontext.jsx

--- frontend/src/context/Storecontext.jsx
+++ frontend/src/context/Storecontext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-1-vgsl.onrender.com";
 
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
